97c35e1b95230f30f1c74bfb69bc4c298c0d23a6
Maxime BELLIER <m4xime.bellier@gmail.com>
2022-03-08 23:12:03 +0100

    Test MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
     siteMetadata: {
+        title: "Oreille Active",
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+              name: `blog`,
+              path: `${__dirname}/actualites`,
+            }
+        },
+        "gatsby-plugin-mdx",
     ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
@@ -9,9 +9,22 @@ import {
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -20,7 +33,12 @@ const Layout = ({ pageTitle, children }) => {
             </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
+            <Link to="/actualites" className={navLinkText}>
+              Actualités
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/a-propos" className={navLinkText}>
               About
             </Link>
           </li>
