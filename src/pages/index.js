67482013783bb09d7cd51a0d8e58f16d3bb32b73
Maxime BELLIER <maximeb85@hotmail.fr>
2022-09-09 16:23:38 +0000

    Ecran d'attente

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import * as React from 'react'
-import Layout from '../components/layout'
+import {
+  container,
+  heading,
+  coverimg,
+  description
+} from './index.css'
 
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </Layout>
-  )
-}
+  <div>
+    <div>
+      <title>L'Oreille Active</title>
+        <link rel="shortcut icon" href="favicon.ico"/>
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet"></link>
+    </div>
+    <div>
+      <div className={container}>
+        <img src="images/icon.png" class={coverimg} alt="Logo"/>
+        <h1 className={heading}>On arrive bientôt...</h1>
+        <p className={description}>L'Oreille Active vous donne rendez-vous bientôt sur son site internet.</p>
+      </div>
+    </div>
+    </div>
+)}
 export default IndexPage
